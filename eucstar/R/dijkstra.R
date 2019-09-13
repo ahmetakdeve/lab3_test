3d2586e842550739e76d49d4aa487d12bdd7b4a2
ahmetakdeve <55084294+ahmetakdeve@users.noreply.github.com>
2019-09-13 11:40:18 +0200

    Documented Dijkstra

--- eucstar/R/dijkstra.R
+++ eucstar/R/dijkstra.R
@@ -1,8 +1,15 @@
-wiki_graph <-
-  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-wiki_graph
+#' Dijkstra's shortest path algoritm
+#'
+#' Dijkstra's algorithm to find the shortest path in a graph
+#'
+#' @param v1 Start node
+#' @param v2 End node
+#' @param w Weighted path between start node and end node
+#'
+#' @return A vector contains the shortest paths from specific start node to other nodes
+#'
+#' @export
+
 
 dijkstra = function(graph, init_node, mpath = FALSE) {
   A<-graph;source<-init_node
